f33281cd94354ec3f5f2b0e0dfbb073e3908d19e
Christian Kappen <christian.kappen@cura4you.de>
2023-02-07 17:54:12 +0100

    docs: add README

--- main.tf
+++ main.tf
@@ -1,10 +1,20 @@
 /**
  * # Terraform AWS Budgets
  *
- * Terraform module which creates an AWS Budgets for you.
+ * This Terraform module provides a preconfigured solution for setting up
+ * AWS Budgets for your AWS account. Our team has years of experience working
+ * with AWS Budgets, and we are sharing our knowledge with you through this
+ * module. With this module, you can quickly and easily set up budgets that
+ * align with your organization's goals and get alerts when your usage exceeds
+ * your specified limits. By using this module, you can save time and avoid
+ * common mistakes when setting up AWS Budgets.
  *
- * The focus on this module lies within it's simplicity by providing default values
- * that should make sense for most use cases.
+ * Not only is this module easy to configure, but it also encapsulates
+ * everything you need in one place. You can simply use the module and be
+ * confident that everything has been set up correctly and efficiently.
+ *
+ * This makes it an excellent choice for organizations of any size looking
+ * to effectively manage their AWS costs.
  */
 resource "aws_budgets_budget" "main" {
   for_each = { for i, budget in var.budgets : budget.name => budget }
